cbda9f4d23d9734ed780278b0b03e100e6f59d98
Chien <xiangjianjian@hotmail.com>
2022-07-16 14:34:38 +0800

    first commit

---
+++ service-provider/src/test/java/top/xiangsanliu/serviceprovider/ServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package top.xiangsanliu.serviceprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
